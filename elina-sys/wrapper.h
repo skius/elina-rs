b866267f9cc0069cdfee3b66a61037eb9383f782
Niels Saurer <me@nielssaurer.com>
2021-10-21 15:00:33 +0200

    Add custom C code functionality to elina-sys

--- elina-sys/wrapper.h
+++ elina-sys/wrapper.h
@@ -1,5 +1,9 @@
 //#define HAS_APRON
 #include "opt_pk.h"
+#include <stdio.h>
 //#include "elina_tcons0.h"
 
 //#include "apron_wrapper.h"
+
+// custom function to iterate over a linexpr0's linterms:
+void foreach_linterm_of_linexpr0(elina_linexpr0_t* e, void (*f) (size_t i, elina_dim_t dim, elina_coeff_t* coeff));
