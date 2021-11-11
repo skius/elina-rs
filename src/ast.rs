30aa1b4f74f51c5aa3f9f59c6dd760fdb3444f9c
Niels Saurer <me@nielssaurer.com>
2021-11-11 03:21:59 +0100

    Fix get_bounds for negative values

--- src/ast.rs
+++ src/ast.rs
@@ -8,7 +8,7 @@ use std::os::raw::c_char;
 use std::ptr::{null_mut, slice_from_raw_parts_mut};
 
 pub use elina_sys::{ConsTyp, TexprBinop, TexprUnop};
-use elina_sys::{__gmpf_floor, __gmpq_get_str, __gmpq_out_str, __gmpz_export, __gmpz_fdiv_q, __mpz_struct, bool_from_c_bool, c_bool_from_bool, elina_abstract0_add_dimensions, elina_abstract0_assign_texpr, elina_abstract0_bottom, elina_abstract0_bound_dimension, elina_abstract0_copy, elina_abstract0_dimension, elina_abstract0_free, elina_abstract0_is_bottom, elina_abstract0_is_eq, elina_abstract0_is_top, elina_abstract0_join, elina_abstract0_meet, elina_abstract0_meet_tcons_array, elina_abstract0_sat_tcons, elina_abstract0_t, elina_abstract0_to_lincons_array, elina_abstract0_top, elina_abstract0_widening, elina_constyp_t, elina_constyp_t_ELINA_CONS_DISEQ, elina_constyp_t_ELINA_CONS_EQ, elina_constyp_t_ELINA_CONS_SUPEQ, elina_dim_t, elina_dimchange_t, elina_interval_free, elina_lincons0_array_clear, elina_lincons0_array_print, elina_manager_free, elina_manager_t, elina_scalar_free, elina_scalar_infty, elina_scalar_t, elina_tcons0_array_make, elina_tcons0_t, elina_texpr0_binop, elina_texpr0_copy, elina_texpr0_cst_interval_top, elina_texpr0_cst_scalar_int, elina_texpr0_dim, elina_texpr0_free, elina_texpr0_t, elina_texpr0_unop, elina_texpr_op_t, elina_texpr_rdir_t_ELINA_RDIR_ZERO, elina_texpr_rtype_t_ELINA_RTYPE_INT, false_, free, opt_pk_manager_alloc, true_};
+use elina_sys::{__gmpf_floor, __gmpq_get_str, __gmpq_neg, __gmpq_out_str, __gmpz_export, __gmpz_fdiv_q, __mpz_struct, bool_from_c_bool, c_bool_from_bool, elina_abstract0_add_dimensions, elina_abstract0_assign_texpr, elina_abstract0_bottom, elina_abstract0_bound_dimension, elina_abstract0_copy, elina_abstract0_dimension, elina_abstract0_free, elina_abstract0_is_bottom, elina_abstract0_is_eq, elina_abstract0_is_top, elina_abstract0_join, elina_abstract0_meet, elina_abstract0_meet_tcons_array, elina_abstract0_sat_tcons, elina_abstract0_t, elina_abstract0_to_lincons_array, elina_abstract0_top, elina_abstract0_widening, elina_constyp_t, elina_constyp_t_ELINA_CONS_DISEQ, elina_constyp_t_ELINA_CONS_EQ, elina_constyp_t_ELINA_CONS_SUPEQ, elina_dim_t, elina_dimchange_t, elina_interval_free, elina_lincons0_array_clear, elina_lincons0_array_print, elina_manager_free, elina_manager_t, elina_scalar_free, elina_scalar_infty, elina_scalar_t, elina_tcons0_array_make, elina_tcons0_t, elina_texpr0_binop, elina_texpr0_copy, elina_texpr0_cst_interval_top, elina_texpr0_cst_scalar_int, elina_texpr0_dim, elina_texpr0_free, elina_texpr0_t, elina_texpr0_unop, elina_texpr_op_t, elina_texpr_rdir_t_ELINA_RDIR_ZERO, elina_texpr_rtype_t_ELINA_RTYPE_INT, false_, free, opt_pk_manager_alloc, true_};
 use crate::util::lincons0_to_string;
 
 /// Provides the implementations of different abstract domains.
@@ -795,7 +795,6 @@ impl Abstract {
                     let inf = *(*inf).val.mpq;
                     let inf_denom = inf._mp_den;
                     let inf_enum = inf._mp_num;
-
                     let mut result_denom = 0u64;
                     let mut result_enum = 0u64;
                     __gmpz_export(
@@ -817,8 +816,15 @@ impl Abstract {
                         &inf_enum,
                     );
 
+                    let str_ptr = __gmpq_get_str(null_mut(), 10, &inf);
+                    let sign = if *str_ptr as u8 == *"-".as_ptr() {
+                        -1
+                    } else {
+                        1
+                    };
+
                     // lower bound, hence we ceil.
-                    Bound::Num(((result_enum as f64) / (result_denom as f64)).ceil() as i64)
+                    Bound::Num(((result_enum as f64) / (result_denom as f64)).ceil() as i64 * sign)
                 }
             };
 
@@ -852,8 +858,15 @@ impl Abstract {
                         &sup_enum,
                     );
 
+                    let str_ptr = __gmpq_get_str(null_mut(), 10, &sup);
+                    let sign = if *str_ptr as u8 == *"-".as_ptr() {
+                        -1
+                    } else {
+                        1
+                    };
+
                     // upper bound, hence we floor.
-                    Bound::Num(((result_enum as f64) / (result_denom as f64)).floor() as i64)
+                    Bound::Num(((result_enum as f64) / (result_denom as f64)).floor() as i64 * sign)
                 }
             };
 
